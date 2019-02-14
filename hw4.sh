0bbb318ff3445368742e943c2b0177a49a055244
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-14 15:13:04 -0800

    wxt

--- hw4.sh
+++ hw4.sh
@@ -1,12 +1,6 @@
 DATAFILE="/scratch/transaction.zip"
-# For testing from head node:
-#DATAFILE="/group/staclassgrp/transaction.zip"
-
-# Example:
 unzip -p ${DATAFILE} | 
     head |
     cat > result.txt
-
-unzip -p ${DATAFILE} | 
-    head -n 2 |
-    cat > result2.txt
+    
+tr ',' '\n' result.txt
