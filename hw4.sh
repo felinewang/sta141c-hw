b34dcfe9385912a655a46873081137b75f08888e
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-16 00:10:48 -0800

    add file

--- hw4.sh
+++ hw4.sh
@@ -1,6 +1,11 @@
 DATAFILE="/scratch/transaction.zip"
+#get the first row, which is all the colums
 unzip -p ${DATAFILE} | 
-    head |
-    cat > result.txt
-    
-tr ',' '\n' result.txt
+    head -n 1 > columns.txt
+
+#seperate the columns to different lines 
+tr ',' '\n' columns.txt > cols.txt
+
+#get the columns' integer positions and store it as requested in HW
+nl cols.txt> colname_index.txt
+
