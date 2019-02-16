47ed8ba0d10815412afc0c62ec134eca532804ff
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-16 01:59:58 -0800

    change

--- hw412.sh
+++ hw412.sh
@@ -1,7 +1,7 @@
 DATAFILE="/group/staclassgrp/transaction.zip"
 #get the first row, which is all the colums
 unzip -p ${DATAFILE} | 
-    head  > text.txt
+    head  > test.txt
 
 #get the length of the longest line in the file 
 wc  --max-line-length  test.txt > maxchars.txt
