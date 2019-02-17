a8574a5e86cbb03f214f47766badf490b26d3eb9
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-16 22:53:42 -0800

    changx

--- hw412.sh
+++ hw412.sh
@@ -4,4 +4,5 @@ unzip -p ${DATAFILE}|
 cut --delimiter="," --fields=8,25| #get the 2 column needed
 awk '$2!=""'|#remove the empty descriptions
 sort --reverse --numeric-sort  --key=1,1 --field-separator=","| #sort transactions
+uniq --unique| #get unique transactions
 head --lines=3 > largest.csv 
