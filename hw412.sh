516c64f3488e34f51e66cd90dc794a2a153c90e3
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-16 17:06:57 -0800

    change

--- hw412.sh
+++ hw412.sh
@@ -1,7 +1,4 @@
-grep -n "funding_agency_id" colname_index.txt|
-cut -d ":" -f 1 > ${AGENCYID} #get the column number of the funding agency ids
-
-cut -d "," -f ${AGNENCYID} data.txt| #get the funding agency id column
+cut -d "," -f 18 data.txt| #get the funding agency id column
 sed 1d| #remove the first row, which is the column name
 sort|#sort the id values so the command "uniq" can get the ids only appearing once
 uniq --unique|#get unique ids
