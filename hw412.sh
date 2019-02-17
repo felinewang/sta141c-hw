40c1196960af1b6204d304b9351523b4e1b8ecd2
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-16 20:59:11 -0800

    changx

--- hw412.sh
+++ hw412.sh
@@ -1,6 +1,9 @@
-cut -d "," -f 18 data.txt| #get the funding agency id column
-sed 1d| #remove the first row, which is the column name
-sort|#sort the id values so the command "uniq" can get the ids only appearing once
-uniq --unique|#get unique ids
-awk "NF" > funding_agency_set.txt #remove empty rows and store the final id numbers
+DATAFILE="/group/staclassgrp/transaction.zip"
+
+
+unzip -p ${DATAFILE}|
+cut --delimiter="," --fields=8, 25| #get the 2 column needed
+awk '$2!=""'|#remove the empty descriptions
+sort --reverse --numeric-sort  --key=1,1 --field-separator=","| #sort transactions
+head -lines=4  > largest.csv #since the file contain column names, to get the 3 largest transactions, select the first 4 rows
 
