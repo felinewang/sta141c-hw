fbf091a8093c45bce23f018e5356cb95f11247ed
Xiaotong Wang <frlwang@ucdavis.edu>
2019-02-22 15:48:30 -0800

    change

--- hw5.sh
+++ hw5.sh
@@ -1,7 +1,9 @@
 DATAFILE="/group/staclassgrp/transaction.zip"
+action_date = 3
+total_obligation = 8
+parent_recipient_unique_id = 52
 
 unzip -p ${DATAFILE}|
 cut --delimiter="," --fields=3,8,52| #get the three columns used to determine duplicates
-sed 1d| #remove the first row, which are the column names
 sort | #sort the rows to make unique work properly
 uniq --unique>unique_transactions.txt #get unique rows
