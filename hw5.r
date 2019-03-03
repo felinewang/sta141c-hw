ce4fb9af6c2b9a849c10f6a31a61c9917b2ffe2d
Xiaotong Wang <frlwang@ucdavis.edu>
2019-03-03 01:37:59 -0800

    change

--- hw5.r
+++ hw5.r
@@ -1,15 +1,23 @@
 file_path = "unique_transactions.txt"
-chunk_size = 1e4L
-lines = readLines(file_path, n = chunk_size)
 output = "first_digit.txt"
 
+transactions_size = 1e4L #take every 1000 transactions at a time
+lines = readLines(file_path, n = transactions_size)
+
 while (0<length(lines)){
-  chunk = read.table(text = lines, sep = ",")
+  #read in the transactions as table to be processed
+  transactions = read.table(text = lines, sep = ",")
   
-  lastcol = ncol(chunk)
+  id_col = 2 #recipient id is in column 2
+  obligation_col = 3 #total obligation isin  column 3
+  transactions = transactions[,id_col:obligation_col] #get only recipient id and total obligation
   
-  write.table(chunk, output,sep = ",",quote = FALSE, col.names = FALSE, row.names = FALSE)
+  #replace the obligation column with the first digit of the total obligation
+  transactions[,obligation_col] = substring(chunk[,lastcol],1L,1L)
   
-  #read the next line
-  lines = readLines(file_path, n = chunk_size)
+  #write results to output
+  write.table(transactions, output,sep = ",",quote = FALSE, col.names = FALSE, row.names = FALSE)
+  
+  #read the next chunk of transactions
+  lines = readLines(file_path, n = transactions_size)
 }
