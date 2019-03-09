35012d7d9f5a6020c2597c89755a6a2826241f90
Xiaotong Wang <frlwang@ucdavis.edu>
2019-03-08 23:44:30 -0800

    change

--- final.sh
+++ final.sh
@@ -3,7 +3,8 @@ DATAFILE="/sta141c-hw4/transaction100.zip"
 
 unzip -p ${DATAFILE}|
 cut --delimiter="," --fields=34,3,12,61,56,42,29| 
-rep -Ev $'^\t|\t\t|\t$'|
+sed 1d| #remove first rows
+grep -Ev $'^\t|\t\t|\t$'|
 awk 'NF >= 6'|# remove empty lines 
 sort | #sort the rows to make unique work properly
 uniq --unique>transactions.txt #get unique transactions and store it to file
