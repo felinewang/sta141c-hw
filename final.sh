1c3532b7fbb6b939396cfd2b579aaaf5db3ba049
Xiaotong Wang <frlwang@ucdavis.edu>
2019-03-08 23:39:18 -0800

    change

--- final.sh
+++ final.sh
@@ -1,14 +1,8 @@
 DATAFILE="/sta141c-hw4/transaction100.zip"
-RECIPIENT_COUNTRY_CODE = 34  
-ACTION_DATE = 3
-FEDERAL_ACTION_OBLIGATION = 12
-BUSINESS_CATEGORIES = 61
-RECIPIEN_NAME = 56
-NASICS_DESCRIPTION = 42
-POP_STATECODE = 29
+
 
 unzip -p ${DATAFILE}|
-cut --delimiter="," --fields=${RECIPIENT_COUNTRY_CODE},${ACTION_DATE},${FEDERAL_ACTION_OBLIGATION},${BUSINESS_CATEGORIES},${RECIPIEN_NAME},${NASICS_DESCRIPTION}| 
+cut --delimiter="," --fields=34,3,12,61,56,42,29| 
 rep -Ev $'^\t|\t\t|\t$'|
 awk 'NF >= 6'|# remove empty lines 
 sort | #sort the rows to make unique work properly
